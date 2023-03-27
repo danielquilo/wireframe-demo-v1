32ee0d6614d76475b4d1e9750cbe519ef9765c91
danielquilo <danielquilo1012@yahoo.com>
2023-03-27 15:38:30 +0100

    done

--- top.js
+++ top.js
@@ -1,14 +1,15 @@
-let user;
+/* let user;
 
 function yourName() {
   user = prompt("What is your name?");
   return document.write(user);
-}
+} 
+
 
 function timeOfDay(param1, param2) {
   let time = prompt("Hi " + user + ", what hour is it? (0 - 23)");
 
-  let message;
+  let message; 
 
   if (time < 12) {
     message = "Good morning " + user + param1;
@@ -22,3 +23,30 @@ function timeOfDay(param1, param2) {
   console.log(message);
   return message;
 }
+*/
+
+function locationLiverpool() {
+  let answer;
+   while (answer !='Liverpool') { 
+    answer = prompt("Where is Liverpool FC located in England?");
+  
+    if (answer !='anfield') {
+      alert ("Try Again");
+    } else {
+      alert("Congratulations, you are correct!");
+    }
+   }
+  }
+  locationLiverpool();
+
+
+function displayChampionsCup() {
+  let output = "";
+  let ChampionsCup = prompt("How many times has Liverpool won the Champions League?");
+  for (let i = 0; i < ChampionsCup; i++) {
+    output += "<img class='trophy' src='imagens/uefa.jpg' alt='cup'>";
+  }s
+  return document.write(output);
+}
+
+displayChampionsCup();
